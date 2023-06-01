b5d3273855669c8953aa185984cb9bcb48bb1449
Mayank Meena <dcryptoniun@gmail.com>
2023-06-01 18:54:54 +0530

    fix

--- pages/docs/test.jsx
+++ pages/docs/test.jsx
@@ -1,11 +1,9 @@
-import { ThemeSwitch } from "../../components/ThemeSwitch";
 import React from "react";
 
 export default function test() {
   return (
     <div>
       <h1>test</h1>
-      <ThemeSwitch />
     </div>
   );
 }
